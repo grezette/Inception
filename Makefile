d5462c73be165a7ceca5d060b565f076c8812f6c
grezette <gqbrielrezette@hotmail.fr>
2021-09-10 15:20:23 +0200

    need to adapt my makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+Volumes=/home/$(USER)/data/dbdata
+Volumes=/home/$(USER)/data/wordpress
+
 all: hosts
+	ifeq(!(shell find ../ -type d -name "data") || !(shell find ../data/ -type d -name "dbdata" ) || !(shell find ../data/ -type d -name "wordpress"))
+		mkdir -p ../data/dbdata mkdir ../data/wordpress
 	docker-compose -f ./srcs/docker-compose.yaml up -d --build
+
 
 hosts:
 	sed -i '1 i\127.0.0.1\grezette.42.fr' /etc/hosts
--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,14 +1,7 @@
 sleep 20
 
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXIST $MYSQL_DATABASE;"
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXIST $WP_ADMIN@'%' IDENTIFIED BY '$WP_ADMIN_PASSWORD';"
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXIST $WP_USER@'%' IDENTIFIED BY '$WP_USER_PASSWORD';"
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO '$WP_ADMIN_USER' IDENTIFIED BY '$WP_ADMIN_PASSWORD';"
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$WP_USER' IDENTIFIED BY '$WP_USER_PASSWORD';"
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-sleep 3
-
-mysqladmin shutdown
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+cd '/usr'
+/usr/bin/mysqld
 
 sleep 3
