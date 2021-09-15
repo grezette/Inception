62198d9c3d7dc98fdc04e7796494fdd0cd920dc1
grezette <gabrielrezette@hotmail.fr>
2021-09-15 21:45:16 +0200

    Need to clean the git and we escape the blackhole

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,17 +1,24 @@
 sleep 15
 
 service mysql start
-# 
-# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-# CREATE USER IF NOT EXISTS '$WP_ADMIN' IDENTIFIED BY '$WP_ADMIN_PASSWORD';
-# CREATE USER IF NOT EXISTS '$WP_USER' IDENTIFIED BY '$WP_USER_PASSWORD';
-# GRANT ALL ON *.* TO '$WP_ADMIN' IDENTIFIED BY '$WP_ADMIN_PASSWORD';
-# GRANT ALL ON $MYSQL_DATABASE.* TO '$WP_USER' IDENTIFIED BY '$WP_USER_PASSWORD';
-# FLUSH PRIVILEGES;" > /tmp/mariadb_user.sql
-# 
-# mysql -u root --password=$MYSQL_ROOT_PASSWORD < /tmp/mariadb_user.sql
-mysql -u root --password=$MYSQL_ROOT_PASSWORD < /tmp/wp-db.sql
-#/usr/bin/mysqld
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$WP_ADMIN' IDENTIFIED BY '$WP_ADMIN_PASSWORD';
+CREATE USER IF NOT EXISTS '$WP_USER' IDENTIFIED BY '$WP_USER_PASSWORD';
+GRANT ALL ON *.* TO '$WP_ADMIN' IDENTIFIED BY '$WP_ADMIN_PASSWORD';
+GRANT ALL ON $MYSQL_DATABASE.* TO '$WP_USER' IDENTIFIED BY '$WP_USER_PASSWORD';
+FLUSH PRIVILEGES;" > /tmp/mariadb_user.sql
+
+mysql -u root --password=$MYSQL_ROOT_PASSWORD < /tmp/mariadb_user.sql
+
+out=$(mysql -e "use DB; show tables")
+if [[ -z $out ]];
+then
+	mysql -u root --password=$MYSQL_ROOT_PASSWORD < /tmp/wp-db.sql
+	echo "db absente"
+else
+	echo "db presente"
+fi
 
 service mysql stop
 
