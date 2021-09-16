d2b3276eeecf72932ec79376169efb594b6da538
grezette <gabrielrezette@hotmail.fr>
2021-09-16 16:16:35 +0200

    Ready to get corrected

--- script.sh
+++
@@ -1,4 +0,0 @@
-if (!(find $HOME/ -type d -name "data") || !(find $HOME/data/ -type d -name "dbdata" ) || !(find $HOME/data/ -type d -name "wordpress"))
-	then mkdir -p $HOME/data/dbdata  $HOME/data/wordpress
-fi
-
